562919ae77ab05393080546964ef6ecf2a57b408
DevendraSangwan <sangwandevendra658@gmail.com>
2026-08-07 10:29:16 +0530

    adding basic aggregation to find data from mongodb

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -139,6 +139,7 @@ const options={
 
   
 })
+
 const logoutUser=asyncHandler(async(req,res)=>{
   //step-1 
   await User.findByIdAndUpdate(req.user._id,
@@ -199,7 +200,6 @@ catch(err){
 }
 
 })
-
 
 const changeCurrentPassword=asyncHandler(async(req,res)=>{
   const {oldPassword,newPassword}=req.body
@@ -268,7 +268,6 @@ const updateUserAvatar=asyncHandler(async(req,res)=>{
   .json(new ApiResponse(200,user,"Avatar details update successfully"))
 })
 
-
 const updateUserCoverImage=asyncHandler(async(req,res)=>{
   const coverImageLocalPath=req.file?.path
   if(!coverImageLocalPath){
@@ -290,4 +289,72 @@ const updateUserCoverImage=asyncHandler(async(req,res)=>{
   .json(new ApiResponse(200,user,"cover Image details update successfully"))
 })
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage};
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+  const {username}=req.params
+  if(!username?.trim()){
+    throw new ApiError(400,"username is missing")
+  }
+   const channel=await User.aggregate([
+    {
+      $match:{
+        username:username?.toLowerCase()
+      }
+    },
+    {
+      $lookup:{
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:"channel",
+        as:"subscribers"
+      }
+    },
+    {
+      $lookup:{
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:"subscriber",
+        as:"subscribedTo"
+      }
+    },
+    {
+      $addFields:{
+        subscribersCount:{
+          $size:"$subscribers",
+        },
+        channelsSubscribedToCount:{
+          $size:"$subscribedTo"
+        },
+        isSubscribed:{
+          $cond:{
+            if:{$in:[req.user?._id,"$subscribers.subscriber"],
+              then:true,
+              else:false,
+            }
+          }
+        }
+      }
+    },
+    {
+      $project:{
+        fullName:1,
+        username:1,
+        subscribersCount:1,
+        channelsSubscribedToCount:1,
+        isSubscribed:1,
+        avatar:1,
+        coverImage:1,
+        email:1
+      }
+    }
+   ])
+   if(!channel?.length){
+    throw new ApiError(404,"channel does not exists")
+   }
+return res
+.status(200)
+.json(new ApiResponse(200,channel[0],"User channel fetched sunccefully "))
+})
+
+
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile};
