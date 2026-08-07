d89f6dfa780ff7ea3257d714e833b50d3bfbf8e2
DevendraSangwan <sangwandevendra658@gmail.com>
2026-08-07 10:42:17 +0530

    adding watch history pipeline (nested pipeline)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -355,6 +355,51 @@ return res
 .json(new ApiResponse(200,channel[0],"User channel fetched sunccefully "))
 })
 
+const getWatchHistory=asyncHandler(async(req,res)=>{
+  const user=await User.aggregate([
+    {
+      $match:{
+        _id:new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullName:1,
+                    username:1,
+                    avatar:1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ] 
+      }
+    },
+  ])
+  return req
+  .status(200)
+  .json(new ApiResponse(200, user[0].watchHistory),"watchHistory fetched successfully.")
+})
 
-
-export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile};
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory};
